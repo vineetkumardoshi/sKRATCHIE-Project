d83c38f886d369bb795ea1a14d6fedd4141394e8
Vineet Kumar Doshi <vineetkumardoshi@users.noreply.github.com>
2014-07-07 18:56:54 +0530

    Update check.js

--- check.js
+++ check.js
@@ -1,33 +1,38 @@
 var tId,pjs,cnt=0;
+
+//initialize 'guess' variable to store the randomly generated hidden number.
 var guess = 0;
 
+//Function to store the hidden number in 'guess' variable.
 $(document).ready(function() {
   pjs = Processing.getInstanceById("processing-skratchie");
-  console.log(cnt+':'+pjs);
+  
   if (!pjs) 
   	tId=setInterval(function() {
     	pjs = Processing.getInstanceById("processing-skratchie");
-    	console.log(cnt+':'+pjs);
+  
     	if (pjs) {
     		guess = pjs.getNumber()
     		clearInterval(tId);}
   		},500);
 });
-	function check(){
-		if(document.getElementById("guess_input").value == guess){
-			
-			$('#remark-success').show().addClass('bounceIn animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-		      $(this).removeClass().addClass('alert alert-success');
-		      $(this).fadeOut(5000);
-		  	});
 
-		}
-		else{
-			
-			$('#remark-wrong').show().addClass('bounceIn animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-      $(this).removeClass().addClass('alert alert-danger');
-      $(this).fadeOut(5000);
+//function to check if the user input is equal to the 'guess' variable.
+function check(){
+	if(document.getElementById("guess_input").value == guess){
+		
+		$('#remark-success').show().addClass('bounceIn animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+	      $(this).removeClass().addClass('alert alert-success');
+	      $(this).fadeOut(5000);
 	  	});
 
-		}
 	}
+	else{
+		
+		$('#remark-wrong').show().addClass('bounceIn animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+		$(this).removeClass().addClass('alert alert-danger');
+		$(this).fadeOut(5000);
+                });
+	
+       }
+}
